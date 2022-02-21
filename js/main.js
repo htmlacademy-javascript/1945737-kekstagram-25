a7d9761f848161f72ad75b77ac8462d6e33cb8b8
olga-surovtseva <olga.surovtseva.94@mail.ru>
2022-02-21 19:46:06 +0500

    внесла изменения в функции RandomInteger и LenghtString

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 function getRandomInteger(min,max) {
-  min = Math.ceil(min);
-  max = Math.trunc(max);
-  if (min<0 || max <= min) {
-    return false;
-  }
-  return Math.trunc(Math.random()*(max-min+1)+min);
-}
-getRandomInteger(0.4,2);
+  min = Math.abs(min);
+  max = Math.abs(max);
+  const lowerValue = Math.ceil(Math.min(min, max));
+  const upperValue = Math.floor(Math.max(min, max));
+  return Math.floor(Math.random()*(upperValue-lowerValue+1)+lowerValue);}
+getRandomInteger(5,2);
 
 
 function maxLenghtString (string,maxLenght) {
-  return(string.length <= maxLenght);
+  return string.length <= maxLenght;
 }
 maxLenghtString('Привет',10);
