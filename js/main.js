72660966ad3ab2a38b2395f17b0f0059cb29902d
olga-surovtseva <olga.surovtseva.94@mail.ru>
2022-03-01 17:09:04 +0500

    корректировка в функциях

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ const getRandomInteger = (min,max) => {
   const upperValue = Math.floor(Math.max(min, max));
   return Math.floor(Math.random()*(upperValue-lowerValue+1)+lowerValue);};
 
+
+function maxLenghtString (string,maxLenght) {
+  return string.length <= maxLenght;
+}
+maxLenghtString('Привет',10);
+
+
 const COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -20,30 +27,41 @@ const NAMES = [
   'Алеша',
 ];
 
+const DESCRIPTIONS = [
+  'Запомнил навсегда!',
+  'Считаю это фото лучшее!',
+  'Как вам это?',
+  'Круто, да?'
+];
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 
-function photoDescription () {
+function photoDescription (_, countPosition) {
+  const randImage = countPosition + 1;
 
   return {
-    id: getRandomInteger (1,25),
-    url: `photos/${getRandomInteger (1,25)}.jpg`,
-    description: 'Было супер!',
+    id: randImage,
+    url: `photos/${randImage}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
     likes: getRandomInteger (15,200),
-    comment: listComments()
+    comment: Array.from({length: getRandomInteger (1,10)}, listComments)
   };
 }
 
 function listComments () {
 
   return {
-    id: getRandomInteger (1,25),
+    id: getRandomInteger (200,300),
     avatar: `img/avatar-${getRandomInteger (1,6)}.svg`,
     message: getRandomArrayElement(COMMENTS),
     name: getRandomArrayElement(NAMES)
   };
 }
 
-const generateIdentifs = Array.from({length: 25},photoDescription);
-generateIdentifs();
+const GENERATED_DATA_AMOUNT = 25;
+const generateIdentifies = Array.from({length: GENERATED_DATA_AMOUNT}, photoDescription);
+
+
+const someFunc = () => generateIdentifies;
+someFunc(); //ESlint
